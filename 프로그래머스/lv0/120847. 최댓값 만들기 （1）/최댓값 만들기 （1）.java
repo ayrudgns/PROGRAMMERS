e1b6ea29cdfb6df5eb519f85d6326b6ea18c56b1
ayrudgns <103123834+ayrudgns@users.noreply.github.com>
2022-12-02 16:59:54 +0900

    Update 최댓값 만들기 （1）.java

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -8,3 +8,23 @@ class Solution {
         return answer;
     }
 }
+// import 없는 
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int max1 = 0;
+        int max2 = 0;
+        for(int i = 1; i < numbers.length; i++) {
+            if(numbers[i] > max2) {
+                if(numbers[i] > max1) {
+                    max1 = numbers[i];
+                    max2 = numbers[i - 1];
+                } else {
+                    max2 = numbers[i];
+                }
+            }
+        }
+        answer = max1 * max2;
+        return answer;
+    }
+}
