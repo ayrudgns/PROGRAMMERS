5c62a42eb9127ef484bc1889c315d09ae063d9d4
ayrudgns <103123834+ayrudgns@users.noreply.github.com>
2022-12-02 10:23:55 +0900

    Update 문자열 뒤집기.java

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -3,12 +3,12 @@ class Solution {
         String answer = "";
         int str_length = my_string.length();
         char[] answer_string = new char[str_length];
-        char[] my_str_arr = my_string.toCharArray();
+        char[] my_str_arr = my_string.toCharArray();            // String을 char[]로 바꾸기
         
         for(int i = 0; i < str_length; i++) {
             answer_string[i] = my_str_arr[my_str_arr.length - 1 - i];
         }
-        answer = new String(answer_string);
+        answer = new String(answer_string);                     // char[]을 String으로 바꾸기
         return answer;
     }
 }
