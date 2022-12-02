5f0ede66fab321fc10d7019010417612922ca31f
ayrudgns <103123834+ayrudgns@users.noreply.github.com>
2022-12-02 13:33:14 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -4,11 +4,11 @@ class Solution {
         char[] str_arr = my_string.toCharArray();
         char[] str_times = new char[my_string.length() * n];
         
-        for(int j = 0; j < n; j++) {
-            for(int i = 0; i < str_arr.length; i++) {        // n번 돌린다.
-                    str_times[(i * n) + j] = str_arr[i];
-            }
-        }
+        for(int j = 0; j < n; j++) {            // n번 돌린다.
+            for(int i = 0; i < str_arr.length; i++) {        // str_arr.length번 돌린다.
+                    str_times[(i * n) + j] = str_arr[i];        // 0, 3, 6, 9
+            }                                                   // 1, 4, 7, 10
+        }                                                       // 위와 같은 방식으로 for문 실행
 
         answer = new String(str_times);
         return answer;
