643dd8cb3cd377bff90fc1f8adc1d2c1a70ea896
ayrudgns <103123834+ayrudgns@users.noreply.github.com>
2023-10-09 15:46:16 +0900

    [level unrated] Title: 문자열 정수의 합, Time: 8.37 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -2,20 +2,13 @@ class Solution {
     public int solution(String num_str) {
         int answer = 0;
         
-        answer = this.jaritsuSum(num_str);
-        
-        return answer;
-    }
-    
-    public int jaritsuSum(String num_str) {
         int[] num_arr = new int[num_str.length()];
-        int sum = 0;
         
         for(int i = 0; i < num_arr.length; i++) {
             num_arr[i] = Integer.parseInt(num_str.charAt(i) + "");
-            sum += num_arr[i];
+            answer += num_arr[i];
         }
         
-        return sum;
+        return answer;
     }
 }
