dfb0d2e458c244c536621f9f2788ece2b0ce3982
ayrudgns <103123834+ayrudgns@users.noreply.github.com>
2024-06-07 16:30:50 +0900

    [level 1] Title: 폰켓몬, Time: 7.54 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,16 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        
         int tempAnswer = Arrays.stream(nums).distinct().toArray().length;
-
-        if((nums.length / 2) < tempAnswer) {
-            answer = (nums.length / 2);
-        } else {
-            answer = tempAnswer;
-        }
         
-        return answer;
+        return (nums.length / 2) < tempAnswer ? (nums.length / 2) : tempAnswer;
     }
 }
