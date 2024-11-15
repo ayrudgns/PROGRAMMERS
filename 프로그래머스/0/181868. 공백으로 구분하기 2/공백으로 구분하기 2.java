db3177a0ab3b6a23205e07c42c6ad04a6ff1cdb6
ayrudgns <103123834+ayrudgns@users.noreply.github.com>
2024-11-15 18:37:43 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 3.11 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -6,7 +6,6 @@ class Solution {
         
         // 문자열을 공백 기준으로 쪼개어 배열로 만든 후, 공백이 아닌 요소만 filter 처리하여 배열로 반환
         Object[] objArray = Arrays.stream(my_string.split(" ")).filter(ans -> !ans.equals("")).toArray();
-        
         // 반환된 배열을 String[] 타입으로 변경
         answer = Arrays.copyOf(objArray, objArray.length, String[].class);
         
